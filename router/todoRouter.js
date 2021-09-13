4c62d366db78d5c7505893ac140d84206ceaafa7
Abeer <abeer.technology@gmail.com>
2021-09-13 22:01:59 +0600

    made the put and delete route

--- router/todoRouter.js
+++ router/todoRouter.js
@@ -26,6 +26,12 @@ router.post("/", async (req, res, next) => {
     ...req.body,
   });
 
+  // validate request
+  if (!req.body) {
+    res.status(400).send({ message: "Content can not be emtpy!" });
+    return;
+  }
+
   try {
     await newTodo.save();
     const todos = await Todo.find();
@@ -40,6 +46,61 @@ router.post("/", async (req, res, next) => {
           msg: "Unknown error occured! 🧨🧨",
         },
       },
+    });
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  if (!req.body) {
+    return res.status(400).send({ message: "Data to update can not be empty" });
+  }
+
+  const id = req.params.id;
+  try {
+    const response = await Todo.findByIdAndUpdate(
+      id,
+      {
+        ...req.body,
+      },
+      {
+        useFindAndModify: false,
+      }
+    );
+    if (!response) {
+      res.status(404).send({
+        message: `Cannot Update user with ${id}. Maybe todo not found!`,
+      });
+    } else {
+      const todos = await Todo.find();
+      res.render("todos", {
+        title: "Task List",
+        todos,
+      });
+    }
+  } catch (err) {
+    res.status(500).send({ message: "Error Update user information" });
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const id = req.params.id;
+
+  try {
+    const response = await Todo.findByIdAndDelete(id);
+    if (!response) {
+      res
+        .status(404)
+        .send({ message: `Cannot Delete with id ${id}. Maybe id is wrong` });
+    } else {
+      const todos = await Todo.find();
+      res.render("todos", {
+        title: "Task List",
+        todos,
+      });
+    }
+  } catch (err) {
+    res.status(500).send({
+      message: "Could not delete todo with id " + id,
     });
   }
 });
